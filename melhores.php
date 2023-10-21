41928545e3979932eeda5326304b8f0b604c3817
Vinicius Souza <vinicipkt@live.com>
2023-10-21 19:44:45 -0300

    Meu commit automático

--- melhores.php
+++ melhores.php
@@ -12,7 +12,7 @@
       </head>
       <body>
         <h1>Dados Ordenados por Melhor Preço por Minuto</h1>
-        <div>last update: 20:16 (UTC)</div>
+        <div>last update: 22:44 (UTC)</div>
         <table>
           <tr>
             <th>ID</th>
@@ -21,10 +21,22 @@
             <th>Valor por Minuto</th>
           </tr>
           <tr>
-            <td><a href="https://openloot.com/items/BT0/Hourglass_Common/issue/537992" target="_blank">537992</a></td>
-            <td>7.39</td>
-            <td>1</td>
-            <td>7.39</td>
+            <td><a href="https://openloot.com/items/BT0/Hourglass_Common/issue/594765" target="_blank">594765</a></td>
+            <td>7.4</td>
+            <td>8</td>
+            <td>0.925</td>
+          </tr>
+          <tr>
+            <td><a href="https://openloot.com/items/BT0/Hourglass_Common/issue/68757" target="_blank">68757</a></td>
+            <td>7.4</td>
+            <td>7</td>
+            <td>1.0571428571428572</td>
+          </tr>
+          <tr>
+            <td><a href="https://openloot.com/items/BT0/Hourglass_Common/issue/100863" target="_blank">100863</a></td>
+            <td>7.4</td>
+            <td>4</td>
+            <td>1.85</td>
           </tr>
         </table>
       </body>
